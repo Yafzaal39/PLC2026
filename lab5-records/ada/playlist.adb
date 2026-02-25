efe125aba3fc79a2dddf7a4119f8bd8ae1b23c91
Yafzaal39 <240136372@aston.ac.uk>
2026-02-25 11:20:13 +0000

    completed tasks in lab 5

--- lab5-records/ada/playlist.adb
+++ lab5-records/ada/playlist.adb
@@ -14,11 +14,19 @@ procedure PlayList is
         Put (To_String (p.name));
     end Put_Person;
 
-    type Item is record
-        name        : Unbounded_String;
-        performer   : Person;
-        length_secs : Float;
-    end record;
+    type item_variant_type is (piece, pause);
+    type Item(item_variant : item_variant_type) is
+        record
+            length_secs : Float;
+            
+            case item_variant is
+                when piece =>
+                    name        : Unbounded_String;
+                    performer   : Person;
+                when pause =>
+                    null;
+            end case;
+        end record;
 
     procedure Put_Item (i : Item) is
     begin
@@ -36,15 +44,15 @@ procedure PlayList is
         length_secs => 17.0*60.0+26.0
        );
 
-    -- pause1 : Item :=
-    --    (
-    --     item_variant => PAUSE,
-    --     length_secs => 5.0
-    --    );
+    pause1 : Item :=
+       (
+        item_variant => PAUSE,
+        length_secs => 5.0
+       );
 
 begin
     Put_Item (piece1);
     Put_Line ("");
-    -- Put_Item(pause1);
-    -- Put_Line ("");
+    Put_Item(pause1);
+    Put_Line ("");
 end PlayList;
